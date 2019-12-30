dfb2a8caeeb7443574d2df6e08e80fcf9bc17239
Paul Moreland <office@Pauls-iPro-2.home>
2019-12-30 13:40:50 +0100

    Script edit

--- content/block/om-projektet.md
+++ content/block/om-projektet.md
@@ -1,3 +1,3 @@
 #### GameOverflow på GitHub
 
-Mitt arbete under projektet till ramverk1: [GitHub repot för projektet](https://github.com/pamo18/ramverk1-projekt).
+Mitt arbete under projektet till ramverk1: [GitHub repot för projektet](https://github.com/pamo18/ramverk1-project).
--- script/post-composer-require.bash
+++ script/post-composer-require.bash
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #
-# Install Weather service into an Anax installation using a default setup.
+# Install Game Overflow into an Anax installation using a default setup.
 #
 rsync -av vendor/pamo18/ramverk1-project/config/ config/
 rsync -av vendor/pamo18/ramverk1-project/content/ content/
 rsync -av vendor/pamo18/ramverk1-project/htdocs/ htdocs/
 rsync -av vendor/pamo18/ramverk1-project/src/ src/
-rsync -av vendor/pamo18/ramverk1-project/test/GeoTag test/
-rsync -av vendor/pamo18/ramverk1-project/test/IPGeotag test/
-rsync -av vendor/pamo18/ramverk1-project/test/IPValidate test/
-rsync -av vendor/pamo18/ramverk1-project/test/Weather test/
+rsync -av vendor/pamo18/ramverk1-project/test/Controller test/Controller/
+rsync -av vendor/pamo18/ramverk1-project/view/ view/
+
+mkdir data
+vendor/pamo18/ramverk1-project/script/reset_db.bash
